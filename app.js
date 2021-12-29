be55427449b48b13ec537cbf2592408ee71cb03d
Alvin <AlvinWang627@gmail.com>
2021-12-29 21:58:10 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -33,6 +33,15 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//detail page
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 mongoose.connect('mongodb://localhost/todo-list') // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
